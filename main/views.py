e33ad1e8f1b976e4a0b231ca6b46a02887e3abdd
Satish NSKY80 <satishkumary80@gmail.com>
2019-10-11 02:06:03 +0530

    backend devloped

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse, Http404, get_object_or_404
+from django.shortcuts import render, HttpResponse, Http404, get_object_or_404, HttpResponseRedirect
 from .models import Question
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -7,18 +8,29 @@ def index(request):
     return render(request, template_name='main/index.html', context={"latest_question_list": latest_question_list})
 
 def detail(request, question_id):
-    """
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")"""
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'main/detail.html', {'question': question})
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'main/results.html', {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, NameError):
+        # Redisplay the question voting form.
+        return render(request, 'main/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('main:results', args=(question.id,)))
