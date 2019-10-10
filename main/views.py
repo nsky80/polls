9bc942b01c9fc2e08ffe138dc89d4069fafe3e35
Satish NSKY80 <satishkumary80@gmail.com>
2019-10-11 00:57:14 +0530

    just minor change

--- main/views.py
+++ main/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, Http404, get_object_or_404
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, template_name='main/index.html', context={"latest_question_list": latest_question_list})
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    """
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")"""
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'main/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
